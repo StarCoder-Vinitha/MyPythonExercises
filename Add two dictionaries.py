26939ef6158ff18f3398f82e84e674641ec9ebfb
StarCoder-Vinitha <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-02-18 21:56:54 +0530

    Add files via upload

---
+++ Add two dictionaries.py
@@ -0,0 +1,6 @@
+first_Dict = {1: 'c', 2: 'c++' , 3: 'java'}
+second_Dict = { 4: 'python', 5: 'datascience'}
+print("Dict_1: ", first_Dict)
+print("Dict_2: ", second_Dict)
+
+print("After Concatenating : ",first_Dict, second_Dict)
