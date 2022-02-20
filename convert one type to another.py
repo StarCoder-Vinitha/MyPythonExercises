0212dc7e1768f2d2d104c33a24e38fbac78897ec
Vinitha Bolle <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-02-21 01:15:55 +0530

    Add files via upload

---
+++ convert one type to another.py
@@ -0,0 +1,20 @@
+x = 5    # int
+y = 2.5  # float
+z = 3j   # complex
+
+#converting int to float:
+a = float(x)
+
+#converting float to int:
+b = int(y)
+
+#convert int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
