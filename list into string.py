18b4c83d2a3dae8687d1882a0ef45eb204970f5e
Vinitha Bolle <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-02-26 16:38:58 +0530

    Add files via upload
    Convert list of  string elements into string using join method

---
+++ list into string.py
@@ -0,0 +1,13 @@
+# list into string with space using join method 
+
+def sample(my_list):
+	string_1 = " "
+	return string_1.join(my_list)
+print(sample(['python','with','datascience']))
+
+# list into string without space using join method
+
+def sample(my_list):
+	string_1 = ""
+	return string_1.join(my_list)
+print(sample(['python','with','datascience']))
